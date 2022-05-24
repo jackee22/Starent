bfeb482ccc45ecc4fe5d1813ef8b47ccaddadbc3
jack hanna <jack_hanna1@hotmail.com>
2022-05-24 12:13:08 +0200

    implemnting the seed with users and stars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 5.times do
+#   user = User.new(
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     manager: true
+#   )
+#   user.save!
+# end
+
+# 5.times do
+#   user = User.new(
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     manager: false
+#   )
+#   user.save!
+# end
+
+1.times do
+  star = Star.new(
+    user_id: 11,
+    name: Faker::Name.name,
+    category: "actor",
+    country: "Belgium",
+    city: "Brussels",
+    price: 200
+  )
+  star.save!
+end
+1.times do
+  star = Star.new(
+    user_id: 12,
+    name: Faker::Name.name,
+    category: "actor",
+    country: "Belgium",
+    city: "Brussels",
+    price: 200
+  )
+  star.save!
+end
+1.times do
+  star = Star.new(
+    user_id: 13,
+    name: Faker::Name.name,
+    category: "actor",
+    country: "Belgium",
+    city: "Brussels",
+    price: 200
+  )
+  star.save!
+end
+1.times do
+  star = Star.new(
+    user_id: 14,
+    name: Faker::Name.name,
+    category: "actor",
+    country: "Belgium",
+    city: "Brussels",
+    price: 200
+  )
+  star.save!
+end
+1.times do
+  star = Star.new(
+    user_id: 15,
+    name: Faker::Name.name,
+    category: "actor",
+    country: "Belgium",
+    city: "Brussels",
+    price: 200
+  )
+  star.save!
+end
