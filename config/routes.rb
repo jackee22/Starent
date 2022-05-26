f40d2a10496718c49799369dba0fe9e827e29ddc
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-05-26 18:07:49 +0200

    reservation index for each user done with view and a little css

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :stars do
     resources :reviews, only: [ :new, :create, :destroy]
-    resources :reservations, only: [ :new, :index, :create, :update, :destroy]
+    resources :reservations, only: [ :new, :create, :update, :destroy]
   end
 
   resources :reviews, only: [:index]
+  resources :reservations, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
