335b389823105e7dc02bf4dbf520aebc1525a019
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-05-27 10:23:02 +0200

    added the final seeds

--- app/models/star.rb
+++ app/models/star.rb
@@ -3,7 +3,7 @@ class Star < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
-  list = %w[actor model athlete singer pornstar influencer youtuber]
+  list = %w[actor model athlete musician pornstar influencer youtuber]
   validates :name, presence: true
   validates :category, presence: true, inclusion: { in: list }
   validates :country, presence: true
